b42166a70407ec39b6aafc6126d61efb0f5b392f
lvtong <lv199881@gmail.com>
2019-02-15 17:12:05 +0800

    挑战练习8.8

--- app/src/main/java/com/a22939/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/a22939/criminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +30,38 @@ public class CrimeListFragment extends Fragment {
         updateUI();
 
         return view;
+    }
+
+    private class PoliceHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private Crime mCrime;
+        private Button mCallPolice;
+
+        public PoliceHolder(LayoutInflater inflater,ViewGroup parent){
+            super(inflater.inflate(R.layout.list_item_police_crime,parent,false));
+            itemView.setOnClickListener(this);
+            mTitleTextView = (TextView)itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView)itemView.findViewById(R.id.crime_date);
+            mCallPolice = (Button)itemView.findViewById(R.id.call_police);
+            mCallPolice.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), "已经联系警察", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(),mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        public void bind(Crime crime){
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+        }
     }
 
     private void updateUI() {
@@ -69,24 +102,52 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
-    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+    private class CrimeAdapter extends RecyclerView.Adapter {
 
         private List<Crime> mCrimes;
+
+        @Override
+        public int getItemViewType(int position) {
+            if (mCrimes.get(position).isRequiresPolice() == true) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
 
         public CrimeAdapter(List<Crime> crimes) {
             mCrimes = crimes;
         }
 
-        @Override
-        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            return new CrimeHolder(layoutInflater, parent);
-        }
 
         @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            if (viewType == 1) {
+                return new PoliceHolder(layoutInflater, parent);
+            } else {
+                return new CrimeHolder(layoutInflater, parent);
+            }
+        }
+
+        /*
+        @Override
+
         public void onBindViewHolder(CrimeHolder holder, int position) {
             Crime crime = mCrimes.get(position);
             holder.bind(crime);
+        }
+        */
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            //得到ViewType 绑定不同的holder
+            if (this.getItemViewType(position) == 1) {
+                ((PoliceHolder) holder).bind(crime);
+            } else {
+                ((CrimeHolder) holder).bind(crime);
+            }
         }
 
         @Override
